c222206a2d52d222b970e288c91abb31ae9919f9
marcelopaula <marcelopaula.rj@gmail.com>
2024-02-22 13:27:49 -0300

    Ordering tasks according to status

--- src/core/components/List/index.tsx
+++ src/core/components/List/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ListItem from '../ListItem'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/core/store'
 import { ItemState } from '@/core/store/slices/list'
 
 const  List = () => {
-  const {data} = useSelector((state:RootState) => state.list);
+  const [tasks, setTasks] = useState<ItemState[]>([])
+  const {data} = useSelector((state:RootState) => state.list)
+
+  useEffect(() => {
+    if (data) {
+      const arrayForSort = [...data]
+      const dataOrder: ItemState[] = arrayForSort.sort((a, b) => {
+        return (a.completed === b.completed)? 0 : a.completed? 1 : -1;
+      })
+      setTasks(dataOrder);
+    }
+  }, [data])
 
   return (
     <div>
       {
-        data.map((item: ItemState, i: number) => 
+        tasks.map((item: ItemState, i: number) => 
           item.visible &&
             <ListItem key={i} {...item} />
         )
